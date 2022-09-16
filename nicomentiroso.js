53d2536741fb10a217e88fbcfb53cdc5b412f81c
Felipe Curso <felipelentini98@hotmail.com>
2022-09-16 20:28:16 -0300

    actualizand oarchivo

---
+++ nicomentiroso.js
@@ -0,0 +1,8 @@
+"use strict";
+
+while (palabraSecreta != "putoelquelolea") {
+    var palabraSecreta = parseInt(prompt("Please enter your palabra secret"))
+    alert ("palabra incorrecta")
+    
+
+    
